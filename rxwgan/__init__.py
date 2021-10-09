01ca71e456cd2b85983974df2c12347c4b3f4db3
jodafons <jodafons@lps.ufrj.br>
2021-10-09 17:04:45 -0300

    update

--- rxwgan/__init__.py
+++ rxwgan/__init__.py
@@ -1,10 +1,7 @@
 __all__ = []
 
-from . import core
-__all__.extend( core.__all__)
-from .core import *
 
-from . import wgan
-__all__.extend( wgan.__all__ )
-from .wgan import *
+#from . import wgan
+#__all__.extend( wgan.__all__ )
+#from .wgan import *
 
