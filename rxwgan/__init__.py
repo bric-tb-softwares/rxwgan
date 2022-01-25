0b52aff06a93f1a1556e5aba3e5718e57dee88e1
jodafons <jodafons@lps.ufrj.br>
2022-01-25 19:43:56 -0300

    fix import

--- rxwgan/__init__.py
+++ rxwgan/__init__.py
@@ -15,10 +15,6 @@ from . import wgangp
 __all__.extend( wgangp.__all__ )
 from .wgangp import *
 
-from . import utils
-__all__.extend( utils.__all__ )
-from .utils import *
-
 from . import plots
 __all__.extend( plots.__all__ )
 from .plots import *
@@ -31,6 +27,3 @@ from . import metrics
 __all__.extend( metrics.__all__ )
 from .metrics import *
 
-from . import kfold
-__all__.extend( kfold.__all__ )
-from .kfold import *
