a55ce8ce26ec3e3dbdc918c502709168ab60c53c
jodafons <jodafons@lps.ufrj.br>
2022-04-22 13:52:34 -0300

    update

--- versions/v1/v1_tb/create_tasks.py
+++ versions/v1/v1_tb/create_tasks.py
@@ -5,24 +5,23 @@ path = basepath
 
 datapath = '/home/jodafons/public/brics_data/Shenzhen/raw/Shenzhen_table_from_raw.csv'
 
-exec_cmd = "source {PATH}/init.sh && "
+exec_cmd = ". {PATH}/init.sh && "
 exec_cmd+= "python {PATH}/run.py -j %IN -i {DATA} && "
-exec_cmd+= "source {PATH}/end.sh"
+exec_cmd+= ". {PATH}/end.sh"
 
 exec_cmd = exec_cmd.format(PATH=basepath, DATA=datapath)
 
 command = """maestro.py task create \
   -v {PATH} \
   -t user.jodafons.task.Shenzhen.wgangp.v1_tb \
-  -c jobs \
+  -i {PATH}/jobs \
   --exec "{EXEC}" \
   """
 
 
 
-command = command.format(PATH=path,EXEC=exec_cmd.format(TEST=test), TEST=test)
-    print(cmd)
-    os.system(cmd)
-    break
+cmd = command.format(PATH=path,EXEC=exec_cmd)
+print(cmd)
+os.system(cmd)
 
 
