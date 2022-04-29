e4942ad1e1ffaf776d4543001dac6eb928d786cc
jodafons <jodafons@lps.ufrj.br>
2022-04-29 11:40:28 -0300

    update

--- rxwgan/models/__init__.py
+++ rxwgan/models/__init__.py
@@ -5,3 +5,7 @@ __all__ = []
 from . import models_v1
 __all__.extend( models_v1.__all__ )
 from .models_v1 import *
+
+from . import models_v2
+__all__.extend( models_v2.__all__ )
+from .models_v2 import *
--- rxwgan/models/models_v2.py
+++ rxwgan/models/models_v2.py
@@ -1,21 +1,20 @@
 
 
-__all__ = ["Generator_v1", "Critic_v1"]
 
-from turtle import width
+__all__ = ["Generator_v2", "Critic_v2"]
+
 from tensorflow.keras import layers
 import tensorflow as tf
 
-height=256
-width =256
 
-class Generator_v1( object ):
+
+class Generator_v2( object ):
 
   def __init__(self, generator_path=None):
 
       self.latent_dim = 100
-      self.height     = height
-      self.width      = width
+      self.height     = 256
+      self.width      = 256
       self.leaky_relu_alpha = 0.3
     
       if generator_path:
@@ -33,27 +32,40 @@ class Generator_v1( object ):
 
   def compile(self):
 
-
       ip = layers.Input(shape=(self.latent_dim,))
-      y = layers.Dense(units=32*32*32, input_shape=(self.latent_dim,))(ip)
-      y = layers.Reshape(target_shape=(32,32, 32))(y)
-      
-      y = layers.Conv2DTranspose(16, (4,4), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
+      # Input (None, latent space (100?) )
+      y = layers.Dense(units=16*16*32, input_shape=(self.latent_dim,))(ip)
+      # Output (None, 64*3^2 )
+      y = layers.Reshape(target_shape=(16,16, 32))(y)
+      #y = layers.BatchNormalization()(y)
+      #y = layers.LeakyReLU(alpha=leaky_relu_alpha)(y)
+      #y = layers.UpSampling1D()(y)
+      # Output (None, 3^2*2, 64)
+      y = layers.Conv2DTranspose(64, (8,8), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
+      y = layers.BatchNormalization()(y)
+      y = layers.LeakyReLU(alpha=self.leaky_relu_alpha)(y)
+      y = layers.Dropout(rate=0.3)(y)
+      #y = layers.UpSampling1D(size=2*2)(y)
+      # Output (None, 3^2*2^3, 128)
+      y = layers.Conv2DTranspose(64, (8,8), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
+      y = layers.BatchNormalization()(y)
+      y = layers.LeakyReLU(alpha=self.leaky_relu_alpha)(y)
+      y = layers.Dropout(rate=0.3)(y)
+      #y = layers.UpSampling1D(size=2*2)(y)
+      # Output (None, 3^2*2^5, 256)
+      y = layers.Conv2DTranspose(128, (8,8), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
       y = layers.BatchNormalization()(y)
       y = layers.LeakyReLU(alpha=self.leaky_relu_alpha)(y)
       y = layers.Dropout(rate=0.3)(y)
 
-      y = layers.Conv2DTranspose(32, (4,4), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
-      y = layers.BatchNormalization()(y)
-      y = layers.LeakyReLU(alpha=self.leaky_relu_alpha)(y)
-      y = layers.Dropout(rate=0.3)(y)
-      
-      y = layers.Conv2DTranspose(64, (4,4), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
+      y = layers.Conv2DTranspose(128, (8,8), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
       y = layers.BatchNormalization()(y)
       y = layers.LeakyReLU(alpha=self.leaky_relu_alpha)(y)
       y = layers.Dropout(rate=0.3)(y)
 
-      out = layers.Conv2DTranspose(1, (4,4), strides=(1,1), padding='same', kernel_initializer='he_uniform', activation = 'tanh')(y)
+      # Output (None, 3^2*2^5, 64)
+      out = layers.Conv2DTranspose(1, (8,8), strides=(1,1), padding='same', kernel_initializer='he_uniform', activation = 'tanh')(y)
+      # Output (None, 3^2*2^5, 1)
       model = tf.keras.Model(ip, out)
       model.compile()
       model.summary()
@@ -61,14 +73,12 @@ class Generator_v1( object ):
 
 
 
-
-
-class Critic_v1( object ):
+class Critic_v2( object ):
 
   def __init__(self, critic_path=None):
 
-      self.height     = height
-      self.width      = width
+      self.height     = 256
+      self.width      = 256
     
       if critic_path:
         self.model = tf.keras.models.load_model(critic_path)
@@ -80,24 +90,30 @@ class Critic_v1( object ):
 
   def compile(self):
 
-
       ip = layers.Input(shape=( self.height,self.width,1))
       # TODO Add other normalization scheme as mentioned in the article
       # Input (None, 3^2*2^5 = 1 day = 288 samples, 1)
-      y = layers.Conv2D(64, (5,5), strides=(2,2), padding='same', kernel_initializer='he_uniform', data_format='channels_last', input_shape=(self.height,self.width,1))(ip)
+      y = layers.Conv2D(128, (10,10), strides=(2,2), padding='same', kernel_initializer='he_uniform', data_format='channels_last', input_shape=(self.height,self.width,1))(ip)
       #y = layers.BatchNormalization()(y)
       y = layers.Activation('relu')(y)
       y = layers.Dropout(rate=0.3, seed=1)(y)
       # Output (None, 3^2*2^3, 64)
-      y = layers.Conv2D(32, (5,5), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
+      y = layers.Conv2D(64, (10,10), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
       #y = layers.BatchNormalization()(y)
       y = layers.Activation('relu')(y)
       y = layers.Dropout(rate=0.3, seed=1)(y)
       # Output (None, 3^2*2^3, 64)
-      y = layers.Conv2D(16, (5,5), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
+      y = layers.Conv2D(64, (10,10), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
       #y = layers.BatchNormalization()(y)
       y = layers.Activation('relu')(y)
       y = layers.Dropout(rate=0.3, seed=1)(y)
+
+      y = layers.Conv2D(32, (10,10), strides=(2,2), padding='same', kernel_initializer='he_uniform')(y)
+      #y = layers.BatchNormalization()(y)
+      y = layers.Activation('relu')(y)
+      y = layers.Dropout(rate=0.3, seed=1)(y)
+
+
       # Output (None, 3^2*2, 128)
       y = layers.Flatten()(y)
       # Output (None, 3*256)
@@ -107,4 +123,3 @@ class Critic_v1( object ):
       model.compile()
       model.summary()
       self.model = model
-
